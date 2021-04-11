6930b28b5b03df2802b489f3977abdb162b2c9e1
Murilo Souza <moslite@hotmail.com>
2021-04-11 09:04:21 -0300

    Project created

---
+++ src/test/java/com/udemy/usermongo/UsermongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.usermongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
