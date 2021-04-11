d6ecfc42c68aa1f9239c6f0226e869fbe1d38d76
Murilo Souza <moslite@hotmail.com>
2021-04-11 10:31:07 -0300

    Using DTO from User entity

--- src/main/java/com/udemy/usermongo/resources/UserResource.java
+++ src/main/java/com/udemy/usermongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.udemy.usermongo.resources;
 
 import com.udemy.usermongo.domain.User;
+import com.udemy.usermongo.dto.UserDTO;
 import com.udemy.usermongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,9 +20,10 @@ public class UserResource {
     UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> userList = userService.findAll();
-        return ResponseEntity.ok().body(userList);
+        List<UserDTO> userDTOList = userList.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(userDTOList);
     }
 
 }
