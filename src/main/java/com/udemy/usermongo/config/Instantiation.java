384eb852ab1e56b4fc64939b56470ebb4dd488cb
Murilo Souza <moslite@hotmail.com>
2021-04-12 06:43:20 -0300

    AuthorDTO created and associated with Post instead User

--- src/main/java/com/udemy/usermongo/config/Instantiation.java
+++ src/main/java/com/udemy/usermongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.udemy.usermongo.config;
 
 import com.udemy.usermongo.domain.Post;
 import com.udemy.usermongo.domain.User;
+import com.udemy.usermongo.dto.AuthorDTO;
 import com.udemy.usermongo.repository.PostRepository;
 import com.udemy.usermongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,8 +35,8 @@ public class Instantiation implements CommandLineRunner {
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 
