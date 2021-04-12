afeeb8850d4bcc90b912c99649b2064bec309c54
Murilo Souza <moslite@hotmail.com>
2021-04-12 06:37:15 -0300

    Entity Post created

---
+++ src/main/java/com/udemy/usermongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.udemy.usermongo.repository;
+
+import com.udemy.usermongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+
+}
