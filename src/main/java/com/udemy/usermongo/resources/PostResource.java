3fa7c62220af36ca4a5fceb8791fc712d0f058b9
Murilo Souza <moslite@hotmail.com>
2021-04-13 06:16:53 -0300

    Simple search with Query Methods

--- src/main/java/com/udemy/usermongo/resources/PostResource.java
+++ src/main/java/com/udemy/usermongo/resources/PostResource.java
@@ -1,13 +1,13 @@
 package com.udemy.usermongo.resources;
 
 import com.udemy.usermongo.domain.Post;
+import com.udemy.usermongo.resources.util.URL;
 import com.udemy.usermongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -20,6 +20,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> postList = postService.findByTitle(text);
+        return ResponseEntity.ok().body(postList);
     }
 
 }
