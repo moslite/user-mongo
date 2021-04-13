82e83d27be9304284ae62c43ddf02a89d4cd3f8f
Murilo Souza <moslite@hotmail.com>
2021-04-13 05:52:17 -0300

    Post findById endpoint

---
+++ src/main/java/com/udemy/usermongo/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.udemy.usermongo.resources;
+
+import com.udemy.usermongo.domain.Post;
+import com.udemy.usermongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    PostService postService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = postService.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+}
---
+++ src/main/java/com/udemy/usermongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.udemy.usermongo.services;
+
+import com.udemy.usermongo.domain.Post;
+import com.udemy.usermongo.repository.PostRepository;
+import com.udemy.usermongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> postOptional = postRepository.findById(id);
+        return postOptional.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+    }
+
+}
